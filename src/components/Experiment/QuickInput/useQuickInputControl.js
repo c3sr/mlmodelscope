7bffb4966990aed12027b6ba36b4a158c5017362
Alex <alexander.lin.kremer@gmail.com>
2024-05-06 16:07:49 +0900

    Added multi-input via urls

--- src/components/Experiment/QuickInput/useQuickInputControl.js
+++ src/components/Experiment/QuickInput/useQuickInputControl.js
@@ -13,7 +13,8 @@ export default function useQuickInputControl(props) {
   const task = Task.getStaticTask(props.model.output.type);
 
   const [selectedInputs, setSelectedInputs] = useState([""]);
-  const [selectedTab, setSelectedTab] = useState(0);
+  // const [selectedTab, setSelectedTab] = useState(0);
+  const [selectedTab, setSelectedTab] = useState(2);
 
   // Note: Uncomment for debugging
   // useEffect(() => {
@@ -72,6 +73,7 @@ export default function useQuickInputControl(props) {
       props.onRunModelClicked(selectedInputs.filter(url => url));
   }
   const selectInput = (url, index) => {
+    console.log('selectInput')
     let selected = selectedInputs;
 
     if (index)
