09907b8380e60db629e5f000d98c5a1d2b2b4a38
Alex <alexander.lin.kremer@gmail.com>
2024-05-08 21:06:16 +0900

    misc cleanup

--- src/components/Experiment/QuickInput/Tabs/SampleInput/useSampleInputControl.js
+++ src/components/Experiment/QuickInput/Tabs/SampleInput/useSampleInputControl.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {QuickInputType} from "../../quickInputType";
 import Task from "../../../../../helpers/Task";
 
--- src/components/Experiment/QuickInput/useQuickInputControl.js
+++ src/components/Experiment/QuickInput/useQuickInputControl.js
@@ -13,8 +13,7 @@ export default function useQuickInputControl(props) {
   const task = Task.getStaticTask(props.model.output.type);
 
   const [selectedInputs, setSelectedInputs] = useState([""]);
-  // const [selectedTab, setSelectedTab] = useState(0);
-  const [selectedTab, setSelectedTab] = useState(2);
+  const [selectedTab, setSelectedTab] = useState(0);
 
   // Note: Uncomment for debugging
   // useEffect(() => {
@@ -73,7 +72,6 @@ export default function useQuickInputControl(props) {
       props.onRunModelClicked(selectedInputs.filter(url => url));
   }
   const selectInput = (url, index) => {
-    console.log('selectInput')
     let selected = selectedInputs;
 
     if (index)
