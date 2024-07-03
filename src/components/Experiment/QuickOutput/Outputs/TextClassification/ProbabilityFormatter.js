ed7befc081d2d116b6aded53c2cbd69b060eb70c
Shivansh Shalabh <shalabhshivansh@gmail.com>
2024-07-03 16:14:02 -0400

    Completed text-classification modality

---
+++ src/components/Experiment/QuickOutput/Outputs/TextClassification/ProbabilityFormatter.js
@@ -0,0 +1,7 @@
+export default function formatProbability(probability) {
+    let result = `${(probability * 100).toFixed()}%`;
+  
+    if (result === "0%") return "<1%";
+  
+    return result;
+  }
---
+++ src/components/Experiment/QuickOutput/Outputs/TextClassification/Rating.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Icon from "../../../../Icon/Icon";
+import "./Rating.scss";
+import useBEMNaming from "../../../../../common/useBEMNaming";
+
+const defaultProps = {
+    className: "rating"
+}
+
+export default function Rating(givenProps) {
+    const props = {...defaultProps, ...givenProps};
+    const {getBlock, getElement} = useBEMNaming(props.className);
+
+    const [ratings, setRatings] = React.useState([
+        {
+            title: "Yes",
+            element: "correct",
+            icon: "check",
+            checked: false
+        },
+        {
+            title: "No",
+            element: "incorrect",
+            icon: "x",
+            checked: false
+        }
+    ]);
+
+    const makeRadioButton = (rating, index) => {
+        const tabIndex = rating.checked ? 0 : (ratings.some(r => r.checked) || index > 0) ? -1 : 0;
+
+        return (
+            <div key={index} className={getElement(rating.element)} role="radio" aria-checked={rating.checked}
+                 tabIndex={tabIndex} onClick={() => clickRating(index)}>
+                <Icon icon={rating.icon}/>
+                <span>{rating.title}</span>
+            </div>
+        )
+    };
+
+    const clickRating = (clickedIndex) => {
+        const updatedRatings = ratings.map((rating, index) => {
+            return {
+                ...rating,
+                checked: clickedIndex === index
+            }
+        });
+
+        setRatings(updatedRatings);
+    };
+
+    const isChecked = ratings.some(rating => rating.checked);
+    if (isChecked)
+        return <div className={getElement("submitted")}>Thank you for your feedback!</div>
+
+    return (
+        <div className={getBlock()}>
+            <div className={getElement('title')}>Are you satisfied with this result?</div>
+            <div className={getElement('buttons')} role="radiogroup">
+                {
+                    ratings.map(makeRadioButton)
+                }
+            </div>
+        </div>
+    );
+}
