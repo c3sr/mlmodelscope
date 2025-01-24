4038a39aa5579e21b5a133e337623c48d2997802
Shivansh Shalabh <shalabhshivansh@gmail.com>
2025-01-24 13:03:36 -0500

    Fix input reference in MultiInputPreview for accurate text display

--- src/components/Experiment/QuickOutput/MultiInputPreview.js
+++ src/components/Experiment/QuickOutput/MultiInputPreview.js
@@ -24,9 +24,9 @@ export default function MultiInputPreview(givenProps) {
   const getInputs = (input) => {
     switch (input.inputType) {
       case TaskInputTypes.Text:
-        let shortened = props.input.src.split(" ").slice(0, 5).join(" ");
-        shortened = shortened + (shortened.length < props.input.src.length ? "..." : "");
-        return <p className={getElement("text")} title={props.input.src}>{shortened}</p>;
+        let shortened = input.src.split(" ").slice(0, 5).join(" ");
+        shortened = shortened + (shortened.length < input.src.length ? "..." : "");
+        return <p className={getElement("text")} title={input.src}>{shortened}</p>;
       case TaskInputTypes.Document:
         return (
           <button className={getElement("document")}>
